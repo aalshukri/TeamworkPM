e8225f0fa7a6904a4c5bfdd0382cbb4424820d92
aalshukri <aalshukri@googlemail.com>
2020-08-17 19:05:55 +0100

    Basic python docker workflow with stubbed teamworkapi setup.

--- run.py
+++ run.py
@@ -1,1 +1,25 @@
-print("Hello World")
+from dotenv import load_dotenv
+import teamwork
+import os
+
+class TeamworkpmStats:
+    """ Class to produce stats on TeamworkPM projects """
+
+    def __init__(self, url, key):        
+        self.teamworkpm_api_url = url
+        self.teamworkpm_api_key = key
+    
+
+    def settings(self):
+        print("teamworpm_api_url = " + self.teamworkpm_api_url)
+        print("teamworkpm_api_key = " + self.teamworkpm_api_key)
+    
+
+#\TeamworkStats
+
+#run
+load_dotenv()
+print("Start")
+p1 = TeamworkpmStats(os.getenv("TEAMWORKPM_API_URL"), os.getenv("TEAMWORKPM_API_KEY"))
+p1.settings()
+
